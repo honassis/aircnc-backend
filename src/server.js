164080e28f950fb46f9f63bd0b11ebabf4c0416a
Honassis Lopes <honassislopes@gmail.com>
2019-10-03 13:13:24 -0300

    novas alterações para ajuste de conexão com o front

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 const routes = require('./routes');
+const path = require('path');
 const app = express();
 mongoose.connect(
 'mongodb://localhost:27017/oministack', 
@@ -13,7 +14,9 @@ useUnifiedTopology :true});
 // req.params = acessar route params (para edição, delete)
 // req.body = acessar corpo da requisição (para criação , edição )
 
+app.use(cors())
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
